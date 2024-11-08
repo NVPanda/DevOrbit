9e82330583a820de8a1f140e60aeedaf59531115
Gilderlan silva da cruz <lansilva007gmail.com>
2024-11-08 14:36:44 -0300

    Foi adicionado um pequeno sistema onde os melhores posts irão aparece no todo da rota feed. Para um post se exibido no topo chamado de 'CodePost' o usurio tera que primeiramente fazer um post na rede, e esse post deve ter mais de 100 ou 300 likes, caso contrario sera exibido a msg padrão

--- application/src/static/js/menu.js
+++ application/src/static/js/menu.js
@@ -17,3 +17,35 @@ fetch('/Codechamber/feed/posts', {
     console.error('Erro na requisição:', error);
     alert('Erro na requisição: ' + error);
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const likeButtons = document.querySelectorAll('.like-button');
+    
+    likeButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const postId = this.getAttribute('data-id');
+            
+            fetch(`posts/${postId}/like`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.likes !== undefined) {
+                    // Atualizar o número de likes no frontend
+                    const likeCountElement = document.getElementById(`like-count-${postId}`);
+                    if (likeCountElement) {
+                        likeCountElement.textContent = `${data.likes} Likes`;
+                    }
+                } else {
+                    console.error('Erro: o campo "likes" não foi encontrado na resposta:', data);
+                }
+            })
+            .catch(error => console.error('Erro ao curtir o post:', error));
+        });
+    });
+});
+
--- teste_requestr_and_try.py
+++ teste_requestr_and_try.py
@@ -1,23 +1,11 @@
-import requests
+usuarios = [
+    {'nome': 'ped', 'like': 20, 'post': 'Hoje é meu primeiro dia como dev jurnio'},
+    {'nome': 'p2', 'like': 10, 'post': 'Hoje é meu primeiro dia como dev senior'},
+    {'nome': 'p3', 'like': 30, 'post': 'Hoje é meu primeiro dia como dev contratado estou muito feliz'},
 
-url = 'localhost:5000/allpost'
+]
 
-def procurando_posts():
-    try:
-        r = requests.get(url, timeout=5)
-        return  f'status code: {r.status_code}: requisição feita com sucesso! :)' if r.ok else f'status_code: {r.status_code}: Não foi porsivel completa a requisição. :('
-    
-    except requests.exceptions.InvalidSchema as error: 
-       return "Erro HTTP ou HTTPS: Você precisa incluir métodos válidos.", error.args[0]
+novos_produtos = [{**produto, 'like': produto['like'] } for produto in usuarios if( produto['like'] >= 25 ) ]
 
-    except requests.exceptions.ReadTimeout as error: 
-         return "HTTP Error" , error.args[0]
-
-        
-
-var = procurando_posts()
-print(var)
-
-
-
-
+for p in novos_produtos:
+    print(f'O Post mais curtido e: {p}')
