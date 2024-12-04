150c0b5a8a01208ccbff22351b9c0e236664f9fb
Gilderlan silva da cruz <lansilva007gmail.com>
2024-12-04 13:17:39 -0300

    add todos os arquivos dentro de src

---
+++ application/src/static/js/animations.js
@@ -0,0 +1,9 @@
+// Função para mostrar o overlay de carregamento
+function showLoading() {
+    const loadingOverlay = document.getElementById('loadingOverlay');
+    loadingOverlay.style.display = 'flex'; // Exibe o overlay
+    setTimeout(() => {
+      window.location.href = '/devorbit/feed/'; // Redireciona para a página home
+    }, 2000); // Redireciona após 2 segundos
+  }
+  
