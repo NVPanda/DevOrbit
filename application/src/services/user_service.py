2f87564d47793c309ca1dfbd643cc65f61fcb05a
Gilderlan silva da cruz <lansilva007gmail.com>
2024-12-14 21:02:30 -0300

    Esta função e usada para busca informação do usuario logado,, com isso vamos evitar suja o codigo fazendo requisição no banco de dados. Tudo agora esta em uma só função get_user_infor. que recebe um parametro obrigatorio que vai verificar se tem um usuario com aquele name/dados em nosso banco.

---
+++ application/src/services/user_service.py
@@ -0,0 +1,31 @@
+from flask import flash, redirect, url_for
+from application.src.database.users.configure_users import my_db
+
+def get_user_info(usuario):
+    banco, cursor = my_db()
+    
+    # Buscar as informações do usuário
+    cursor.execute(
+        'SELECT id, photo, bio, github, likedin, site, followers, following, banner, name FROM usuarios WHERE name = ?',
+        (usuario,)
+    )
+    user = cursor.fetchone()
+
+    if not user:
+        flash('Usuário não encontrado.', 'error')
+        return redirect(url_for('home.home_page'))  # Redireciona caso o usuário não seja encontrado
+
+    # Retornar diretamente um dicionário com as informações
+    return [{
+        'user_photo': user[1],
+        'bio': user[2],
+        'github': user[3],
+        'linkedin': user[4],
+        'site': user[5],
+        'followers': user[6],
+        'following': user[7],
+        'banner': user[8],
+        'name': user[9]
+    }]
+
+    
